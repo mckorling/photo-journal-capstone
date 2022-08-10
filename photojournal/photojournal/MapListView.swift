0496dd2adf2bd5a14d5f81dfcca45bed16903a80
Megan Korling <korlingm@gmail.com>
2022-08-10 13:10:07 -0700

    map annotations added. but lat and lon for each entry revert back to 0 when reopened

--- photojournal/photojournal/MapListView.swift
+++ photojournal/photojournal/MapListView.swift
@@ -6,12 +6,45 @@
 //
 
 import SwiftUI
+import MapKit
 
+struct EntryAnnotation: Identifiable {
+    let id = UUID()
+    let entry: Entry
+    let coordinates: CLLocationCoordinate2D
+}
 
 struct MapListView: View {
-
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.date, order: .reverse)]) var entries: FetchedResults<Entry>
+    
+    func setNewArr(entriesArr: FetchedResults<Entry>) -> [EntryAnnotation] {
+        var entryAnnotationArr = [EntryAnnotation]()
+        for entry in entriesArr {
+            let newAnnotation = EntryAnnotation(entry: entry, coordinates: CLLocationCoordinate2D(latitude: entry.latitude, longitude: entry.longitude))
+            entryAnnotationArr.append(newAnnotation)
+        }
+        return entryAnnotationArr
+    }
+    
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 47.6062, longitude: -122.3321), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
     var body: some View {
-        Text("Mapkit View")
+        let formattedEntries = setNewArr(entriesArr: entries)
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: formattedEntries) { entry in
+                MapAnnotation(coordinate: entry.coordinates) {
+                    NavigationLink {
+                        SingleEntryView(entry: entry.entry)
+                    } label: {
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 25, height: 25)
+                    }
+                }
+            }
+            .ignoresSafeArea()
+        }
+        
     }
 }
 
